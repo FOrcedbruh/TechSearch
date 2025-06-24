66f1d212c9e46a634848527f56aea8934cb08051
Илья Чапля <_ilchpl_@MacBook-Air-Ila.local>
2025-06-24 08:44:52 +0300

    add extra fields to responses.py

--- app/dto/selection_vehicle/responses.py
+++ app/dto/selection_vehicle/responses.py
@@ -6,3 +6,6 @@ class ManufacturerModel(BaseModel):
     id: int = Field(alias="MFA_ID")
     brand: str = Field(alias="MFA_BRAND")
     models_count: int = Field(alias="MFA_MODELS_COUNT")
+
+    class Config:
+        extra = True
